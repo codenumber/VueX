90792bb207596d35c10fc4c567c59aa3a56c0f51
SC-201804152013\Administrator <1353682267@qq.com>
2020-06-21 16:23:04 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-addtration></my-addtration>
+    <p>______________________</p>
+    <my-subtration></my-subtration>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Addition from './components/Addition.vue'
+import Subtration from './components/Subtration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'my-addtration': Addition,
+    'my-subtration': Subtration
   }
 }
 </script>
---
+++ src/components/Subtration.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button> - 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
